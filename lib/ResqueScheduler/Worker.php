79ef21c46a7e27e1a1282e83fb8d08087f2996ab
cutoffurmind <archangel9284@gmail.com>
2016-11-17 00:39:09 +0300

    fix

--- lib/ResqueScheduler/Worker.php
+++ lib/ResqueScheduler/Worker.php
@@ -7,7 +7,7 @@
  * @copyright	(c) 2012 Chris Boulton
  * @license		http://www.opensource.org/licenses/mit-license.php
  */
-namespace resque\lib;
+namespace resque\lib\Resque;
 
 use resque\lib\Resque;
 use resque\lib\Resque\Resque_Stat;
